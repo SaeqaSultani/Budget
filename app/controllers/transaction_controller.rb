470b3db26d30e86721c9399035bc4141dd1701bc
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-29 18:38:26 +0430

    Create cagories , add categoris,transactions , add transactions

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,9 +1,50 @@
 class TransactionController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
 
-  def create; end
+  def index
+    @group = Group.find(params[:category_id])
+    @transactions = @group.categories
+    @categories = current_user.groups.all
+  end
 
-  def new; end
+  def new
+    @transaction = Category.new
+    @group = Group.new
+    @categories = current_user.groups.all
+  end
 
-  def destroy; end
+
+  def create
+    @transaction = Category.new(transaction_params)
+    @transaction.user_id = current_user.id
+    if @transaction.save
+      @group_categories = GroupCategory.new(group_id: params[:category_id], category_id: @transaction.id)
+      if @group_categories.save
+        redirect_to category_transaction_index_path(params[:category_id]), notice: 'Successfully created transaction.'
+      else
+        render :new
+      end
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    group = Group.find(params[:category_id])
+    @transaction = group.categories.find(params[:id])
+    category = Category.find(@transaction.id)
+    category.group_categories.find_by(category_id: category.id).destroy
+    @transaction.destroy
+    if @transaction.destroy
+      redirect_to category_transaction_index_path(params[:category_id])
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def transaction_params
+    params.require(:category).permit(:name, :amount)
+  end
 end

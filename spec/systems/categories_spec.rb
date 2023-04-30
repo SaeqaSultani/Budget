b9992037365c6f59bf7947a4791ced3dfe087730
Saeqa Sultani <saeqasultani@gmail.com>
2023-04-30 16:12:29 +0430

    Add categories test for system

---
+++ spec/systems/categories_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :system do
+  user = User.create!(name: 'Sahil', email: 'Sahil@gmail.com', password: '123456')
+group = Group.create!(user_id: user.id, name: 'test', icon: 'burger.png')
+
+  subject do
+    Category.new(user_id: user.id, name: 'test', amount: 20)
+  end
+  before do
+    subject.save
+  end
+
+  describe 'index page' do
+    it "I can see the categories's name" do
+      visit category_transaction_index_path(group.id)
+      page.has_content?(subject.name)
+    end
+
+    it "I can see the categories's amount." do
+      visit category_transaction_index_path(group.id)
+      page.has_content?(subject.amount)
+    end
+  end
+end
